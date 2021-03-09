3f2bfd3890e8f4c31f778656de937538acd5d29a
Robert Rosentrater <rorosentrater@hotmail.com>
2021-03-08 22:48:05 -0500

    Adding script to make env file using env vars

---
+++ src/environments/set-env.ts
@@ -0,0 +1,26 @@
+// import { writeFile } from 'fs';
+const fs = require('fs');
+const writeFile = fs.writeFile;
+
+// Configure Angular `environment.ts` file path
+const targetPath = './src/environments/environment.ts';
+
+// Load node modules
+const colors = require('colors');
+require('dotenv').load();
+
+// `environment.ts` file structure
+const envConfigFile = `export const environment = {
+   backendEndpoint: '${process.env.BACKEND_ENDPOINT}',
+   production: false
+};
+`;
+console.log(colors.magenta('The file `environment.ts` will be written with the following content: \n'));
+console.log(colors.grey(envConfigFile));
+writeFile(targetPath, envConfigFile, function (err) {
+  if (err) {
+    throw console.error(err);
+  } else {
+    console.log(colors.magenta(`Angular environment.ts file generated correctly at ${targetPath} \n`));
+  }
+});
